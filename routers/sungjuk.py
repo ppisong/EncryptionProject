78b06a7e2248dc72db038717845f8fe59f390d87
hypo <ppisong@gmail.com>
2026-01-16 11:17:33 +0900

    라우터 기반 성적처리 앱 성적리스트 구현

--- routers/sungjuk.py
+++ routers/sungjuk.py
@@ -7,7 +7,27 @@ router = APIRouter(prefix="/sungjuk", tags=["sungjuk"])
 
 @router.get("/list", response_class=HTMLResponse)
 async def sungjuk_list(request: Request):
-    pass
+    async with aiosqlite.connect(SungJukDB_NAME) as db:
+        results = await db.execute_fetchall("""
+            SELECT sjno, name, kor, eng, mat
+            FROM sungjuk ORDER BY sjno DESC""")
+
+    # 조회된 결과집합을 html에서 처리하기 편하게 JSON형식으로 변환
+    sungjuks = []
+    for rs in results:
+        sungjuk = {
+            "sjno": rs[0],
+            "name": rs[1],
+            "kor": rs[2],
+            "eng": rs[3][:10],  # 년월일만 추출
+            "mat": rs[4]
+        }
+        sungjuks.append(sungjuk)
+
+    return templates.TemplateResponse("sungjuk/sungjuk_list.html", {
+        "request": request,
+        "sungjuks": sungjuks
+    })
 
 @router.get("/new", response_class=HTMLResponse)
 async def sungjuk_newform(request: Request, ):
